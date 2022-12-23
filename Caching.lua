b541a497c9aaa97ffd299d745a1ed8d9b8bf8e37
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2022-12-23 18:04:54 +0100

    Revert "Work"
    This reverts commit a960a34adf0d85dbfc08e4ce5c95e0b878b4c8b6.

--- Caching.lua
+++ Caching.lua
@@ -1,21 +1,13 @@
-local addOnName = ...
-local version = '1.0.0'
+Caching = {}
 
-if not Library.isRegistered(addOnName, version) then
-  --- @class Caching
-  local Caching = {}
-
-  Library.register(addOnName, version, Caching)
-
-  function Caching.cached(fn)
-    local hasResultBeenCached = false
-    local cachedResult = nil
-    return function(...)
-      if not hasResultBeenCached then
-        cachedResult = { fn(...) }
-        hasResultBeenCached = true
-      end
-      return unpack(cachedResult)
+function Caching.cached(fn)
+  local hasResultBeenCached = false
+  local cachedResult = nil
+  return function(...)
+    if not hasResultBeenCached then
+      cachedResult = { fn(...) }
+      hasResultBeenCached = true
     end
+    return unpack(cachedResult)
   end
 end
