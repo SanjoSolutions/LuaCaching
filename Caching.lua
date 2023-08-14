596ec24214fed9ebc7b660b5f8733ec8497e87ff
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2023-08-14 13:53:24 +0200

    Use Library, adds package.json and removes .dependencies

--- Caching.lua
+++ Caching.lua
@@ -1,13 +1,23 @@
-Caching = {}
+local addOnName = 'Caching'
+local version = '1.0.0'
 
-function Caching.cached(fn)
-  local hasResultBeenCached = false
-  local cachedResult = nil
-  return function(...)
-    if not hasResultBeenCached then
-      cachedResult = { fn(...) }
-      hasResultBeenCached = true
+if _G.Library and not Library.isRegistered(addOnName, version) then
+  --- @class Caching
+  local Caching = {}
+
+  function Caching.cached(fn)
+    local hasResultBeenCached = false
+    local cachedResult = nil
+    return function(...)
+      if not hasResultBeenCached then
+        cachedResult = { fn(...) }
+        hasResultBeenCached = true
+      end
+      return unpack(cachedResult)
     end
-    return unpack(cachedResult)
   end
+
+  Library.register(addOnName, version, Caching)
+else
+  error(addOnName + ' requires Library. It seems absent.')
 end
